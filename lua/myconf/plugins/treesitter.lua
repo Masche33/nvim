f1600e44d11200fc703fba6fef2ba5044144e37a
Masche33 <matteo.mascherpa1@studenti.unimi.it>
2025-02-28 15:41:14 +0100

    Nvim-tree, Treesitter, now works

---
+++ lua/myconf/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+    "nvim-treesitter/nvim-treesitter",  -- Aggiungi il plugin treesitter
+    run = ":TSUpdate",  -- Aggiorna i parser se necessario
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            ensure_installed = { "lua", "python", "javascript" },  -- Aggiungi qui gli altri linguaggi che usi
+            highlight = {
+                enable = true,  -- Abilita la colorazione della sintassi
+                disable = {},  -- Puoi disabilitare linguaggi specifici qui, se necessario
+            },
+            indent = {
+                enable = true,  -- Abilita l'indentazione automatica
+            },
+        }
+    end
+}
