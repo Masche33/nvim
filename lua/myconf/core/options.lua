dfa45339c20ad4de432cd89f0ef3a9642ec78f61
Masche33 <matteo.mascherpa1@studenti.unimi.it>
2025-03-09 18:48:20 +0100

    Minor changes

--- lua/myconf/core/options.lua
+++ lua/myconf/core/options.lua
@@ -5,7 +5,7 @@ opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
-opt.wrap = false
+opt.wrap = true
 
 -- line numbers
 opt.number = true
--- lua/myconf/plugins/treesitter.lua
+++ lua/myconf/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     run = ":TSUpdate",  -- Aggiorna i parser se necessario
     config = function()
         require'nvim-treesitter.configs'.setup {
-            ensure_installed = { "lua", "python", "javascript" },  -- Aggiungi qui gli altri linguaggi che usi
+            ensure_installed = { "lua", "python", "javascript", "markdown" },  -- Aggiungi qui gli altri linguaggi che usi
             highlight = {
                 enable = true,  -- Abilita la colorazione della sintassi
                 disable = {},  -- Puoi disabilitare linguaggi specifici qui, se necessario
